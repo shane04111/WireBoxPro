660dbc41d73d1f5c86a53a47810cd783a785cb6f
Gregory Brutsche <gregory@mail.brutsches.local>
2021-01-27 18:31:39 -0500

    Add per-player mode to change which wires are placed.

--- control.lua
+++ control.lua
@@ -10,16 +10,22 @@ end
 
 local function on_player_selected_area(event)
     if event.item == "wire-box-tool" then
+        local playerSettings = settings.get_player_settings(event.player_index)
+
         for k, entity in pairs(event.entities) do
             if entity.type == "electric-pole" and entity.neighbours and entity.neighbours.copper then
                 for k, targetEntity in pairs(entity.neighbours.copper) do
                     -- Ignore entities we're connected to that aren't in our selected entities.
                     if getEntityOrNilFromDict(event.entities, targetEntity) then
-                        if not getEntityOrNilFromDict(entity.neighbours.red, targetEntity) then
+                        local isBoth = playerSettings["wire-box-tool-mode"].value == "red-green"
+                        local isRed = isBoth or playerSettings["wire-box-tool-mode"].value == "red-only"
+                        local isGreen = isBoth or playerSettings["wire-box-tool-mode"].value == "green-only"
+
+                        if isRed and not getEntityOrNilFromDict(entity.neighbours.red, targetEntity) then
                             entity.connect_neighbour({wire = defines.wire_type.red, target_entity = targetEntity})
                         end
 
-                        if not getEntityOrNilFromDict(entity.neighbours.green, targetEntity) then
+                        if isGreen and not getEntityOrNilFromDict(entity.neighbours.green, targetEntity) then
                             entity.connect_neighbour({wire = defines.wire_type.green, target_entity = targetEntity})
                         end
                     end
---
+++ settings.lua
@@ -0,0 +1,9 @@
+data:extend({
+    {
+        type = "string-setting",
+        setting_type = "runtime-per-user",
+        default_value = "red-green",
+        name = "wire-box-tool-mode",
+        allowed_values = { "red-green", "red-only", "green-only" },
+    }
+})
